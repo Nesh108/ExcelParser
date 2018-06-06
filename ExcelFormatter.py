63a1ffcc713c5a9ed07da7a4cc0da989eefa3112
nesh <kulplex@gmail.com>
2018-06-06 23:42:33 +0530

    added checks for numbers and ranges

--- ExcelFormatter.py
+++ ExcelFormatter.py
@@ -145,24 +145,58 @@ class ExcelFormatter(object):
 							errors += 1
 		return errors
 
-	# def CheckCountOrRanger(self, column_names):
-	# 	errors = 0
-	# 	for idx in self.GetColumnIndicesByName(column_names):
-	# 		for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
-	# 			for cell in islice(selected_column, 2, None):
-	# 				if cell.value is None:
-	# 					# check adjacent row
+	def CheckNumberRangeLines(self, column_names):
+		errors = 0
+		column_number_idx = self.GetColumnIndicesByName(column_names)[0]
+		column_range_idx = self.GetColumnIndicesByName(column_names)[1]
+		cell_index = 3
+		for index, selected_column in enumerate(islice(self.ws.iter_cols(), column_number_idx, column_number_idx+1)):
+			for cell in islice(selected_column, 2, None):
+				range_cell = self.ws.cell(row=cell_index, column=column_range_idx + 1)
+				errors += self.CheckCountOrRange(cell, range_cell)
+				cell_index += 1
+		return errors
 
-	# 					# if adjacent row is empty and not "NA"
-	# 						 # fill both cells
-	# 						self.fillCellColour(cell, self.color_red)
-	# 						errors += 1
-	# 				elif self.isNotAvailable(cell.value.strip()):
-	# 					cell.value = "NA"
-	# 				elif not re.match(r"^(\d{1,5})$", str(cell.value)) and str(cell.value) != "NA":
-	# 					self.fillCellColour(cell, self.color_yellow)
-	# 					errors += 1
-	# 	return errors
+	def CheckCountOrRange(self, cell_number, cell_range):
+		if cell_number.value is None:
+			if cell_range.value is None:
+				self.fillCellColour(cell_number, self.color_red)
+				self.fillCellColour(cell_range, self.color_red)
+				return 1
+			else:
+				if not re.match(r"^[']?\d{1,10}-\d{1,10}$", str(cell_range.value).strip()) and str(cell_range.value).strip() != "NA":
+					self.fillCellColour(cell_range, self.color_yellow)
+					return 1
+		elif not re.match(r"^\d{1,10}$", str(cell_number.value).strip()):
+			self.fillCellColour(cell_number, self.color_yellow)
+			return 1
+		return 0
+
+	def CheckSpecialitiesLines(self, column_names):
+		errors = 0
+		column_spec_idx = self.GetColumnIndicesByName(column_names)[0]
+		column_number_idx = self.GetColumnIndicesByName(column_names)[1]
+		column_range_idx = self.GetColumnIndicesByName(column_names)[2]
+		cell_index = 3
+		for index, selected_column in enumerate(islice(self.ws.iter_cols(), column_spec_idx, column_spec_idx+1)):
+			for cell in islice(selected_column, 2, None):
+				number_cell = self.ws.cell(row=cell_index, column=column_number_idx + 1)
+				range_cell = self.ws.cell(row=cell_index, column=column_range_idx + 1)
+				if str(cell.value).strip().upper() == "YY":
+					cell.value = "YY"
+					errors += self.CheckCountOrRange(number_cell, range_cell)
+				elif str(cell.value).strip().upper() == "NA" or str(cell.value).strip().upper() == "NO":
+					cell.value = "NA"
+					if number_cell.value is not None:
+						self.fillCellColour(number_cell, self.color_red)
+						errors += 1
+					if str(range_cell.value).strip() != "NA":
+						self.fillCellColour(range_cell, self.color_red)
+						errors += 1
+				else:
+					self.fillCellColour(cell, self.color_red)
+				cell_index += 1
+		return errors
 
 	def CheckYYNOAPPLines(self, column_names):
 		errors = 0
