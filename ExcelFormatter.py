6bd7b06c175e594a0acd10507694fc359edd5715
nesh <kulplex@gmail.com>
2018-06-06 19:04:45 +0530

    added new formatters

--- ExcelFormatter.py
+++ ExcelFormatter.py
@@ -37,12 +37,10 @@ class ExcelFormatter(object):
 			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
 				for cell in islice(selected_column, 2, None):
 					if cell.value is None:
-						cell.fill = PatternFill(start_color=self.color_red, end_color=self.color_red, fill_type='solid')
+						self.fillCellColour(cell, self.color_red)
 						errors += 1
 					elif not cell.value.strip().endswith(','):
-						cell.fill = PatternFill(start_color=self.color_yellow, end_color=self.color_yellow, fill_type='solid')
 						cell.value = cell.value.strip() + ','
-						errors += 1
 					else:
 						cell.value = cell.value.strip()
 		return errors
@@ -53,12 +51,12 @@ class ExcelFormatter(object):
 			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
 				for cell in islice(selected_column, 2, None):
 					if cell.value is None:
-						cell.fill = PatternFill(start_color=self.color_red, end_color=self.color_red, fill_type='solid')
+						self.fillCellColour(cell, self.color_red)
 						errors += 1
-					elif cell.value.strip() == "NI" or cell.value.strip() == "NA" or cell.value.strip() == "N0"  or cell.value.strip() == "NP":
+					elif self.isNotAvailable(cell.value.strip()):
 						cell.value = "NO"
 					elif not re.match(r"[^@]+@[^@]+\.[^@]+", cell.value.strip()) and cell.value.strip() != "NO":
-						cell.fill = PatternFill(start_color=self.color_yellow, end_color=self.color_yellow, fill_type='solid')
+						self.fillCellColour(cell, self.color_yellow)
 						errors += 1
 					else:
 						cell.value = cell.value.strip()
@@ -70,13 +68,46 @@ class ExcelFormatter(object):
 			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
 				for cell in islice(selected_column, 2, None):
 					if cell.value is None:
-						cell.fill = PatternFill(start_color=self.color_red, end_color=self.color_red, fill_type='solid')
+						self.fillCellColour(cell, self.color_red)
 						errors += 1
-					elif cell.value.strip() == "NI" or cell.value.strip() == "NA" or cell.value.strip() == "N0"  or cell.value.strip() == "NP":
+					elif self.isNotAvailable(cell.value.strip()):
 						cell.value = "NO"
 					elif not re.match(r"^(\+\d{1,3}\ \d{10})$", cell.value.strip()) and cell.value.strip() != "NO":
-						cell.fill = PatternFill(start_color=self.color_yellow, end_color=self.color_yellow, fill_type='solid')
+						# Replacing invisible non-really-a-space
+						new_str = str(cell.value).replace(' ', '-')
+						p = re.compile(r"^(\+\d{1,3}\-\d{10})$")
+						found_val = p.search(new_str)
+						if found_val is not None:
+							cell.value = found_val.group(0).replace('-', ' ')
+						else:
+							self.fillCellColour(cell, self.color_yellow)
+							errors += 1
+					else:
+						cell.value = cell.value.strip()
+		return errors
+
+	def CheckTitleLines(self, column_names):
+		errors = 0
+		for idx in self.GetColumnIndicesByName(column_names):
+			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
+				for cell in islice(selected_column, 2, None):
+					if cell.value is None:
+						self.fillCellColour(cell, self.color_red)
 						errors += 1
+					elif not (cell.value.strip() in ["Mr.", "Dr.", "Mrs.", "Ms.", "Adv."]):
+						if "mrs" in cell.value.strip().lower():
+							cell.value = "Mrs."
+						elif "mr" in cell.value.strip().lower():
+							cell.value = "Mr."
+						elif "ms" in cell.value.strip().lower():
+							cell.value = "Ms."
+						elif "dr" in cell.value.strip().lower():
+							cell.value = "Dr."
+						elif "adv" in cell.value.strip().lower():
+							cell.value = "Adv."
+						else:
+							self.fillCellColour(cell, self.color_yellow)
+							errors += 1
 					else:
 						cell.value = cell.value.strip()
 		return errors
@@ -87,11 +118,87 @@ class ExcelFormatter(object):
 			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
 				for cell in islice(selected_column, 2, None):
 					if cell.value is None:
-						cell.fill = PatternFill(start_color=self.color_red, end_color=self.color_red, fill_type='solid')
+						self.fillCellColour(cell, self.color_red)
 						errors += 1
-					elif not isinstance(cell.value, float) and (cell.value.strip() == "NO" or cell.value.strip() == "NA" or cell.value.strip() == "N0"  or cell.value.strip() == "NP"):
+					elif not isinstance(cell.value, float) and self.isNotAvailable(cell.value.strip()):
 						cell.value = "NI"
 					elif not re.match(r"^(\d{1,2}.\d{4,10} \d{1,3}.\d{4,10})$", str(cell.value)) and str(cell.value) != "NI":
-						cell.fill = PatternFill(start_color=self.color_yellow, end_color=self.color_yellow, fill_type='solid')
+						self.fillCellColour(cell, self.color_yellow)
 						errors += 1
 		return errors
+
+	def CheckHoursLines(self, column_names):
+		errors = 0
+		for idx in self.GetColumnIndicesByName(column_names):
+			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
+				for cell in islice(selected_column, 2, None):
+					if cell.value is None:
+						self.fillCellColour(cell, self.color_red)
+						errors += 1
+					elif not re.match(r"^([01]?[0-9]|2[0-3]):[0-5][0-9]$", str(cell.value)):
+						p = re.compile(r"([01]?[0-9]|2[0-3]):[0-5][0-9]")
+						found_val = p.search(str(cell.value))
+						if found_val is not None:
+							cell.value = found_val.group(0)
+						else:
+							self.fillCellColour(cell, self.color_yellow)
+							errors += 1
+		return errors
+
+	# def CheckCountOrRanger(self, column_names):
+	# 	errors = 0
+	# 	for idx in self.GetColumnIndicesByName(column_names):
+	# 		for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
+	# 			for cell in islice(selected_column, 2, None):
+	# 				if cell.value is None:
+	# 					# check adjacent row
+
+	# 					# if adjacent row is empty and not "NA"
+	# 						 # fill both cells
+	# 						self.fillCellColour(cell, self.color_red)
+	# 						errors += 1
+	# 				elif self.isNotAvailable(cell.value.strip()):
+	# 					cell.value = "NA"
+	# 				elif not re.match(r"^(\d{1,5})$", str(cell.value)) and str(cell.value) != "NA":
+	# 					self.fillCellColour(cell, self.color_yellow)
+	# 					errors += 1
+	# 	return errors
+
+	def CheckYYNOAPPLines(self, column_names):
+		errors = 0
+		for idx in self.GetColumnIndicesByName(column_names):
+			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
+				for cell in islice(selected_column, 2, None):
+					if cell.value is None:
+						self.fillCellColour(cell, self.color_red)
+						errors += 1
+					elif cell.value.strip().lower() == "yy" or cell.value.strip().lower() == "app":
+						cell.value = "YY"
+					elif cell.value.strip().lower() == "no":
+						cell.value = "NO"
+					else:
+						self.fillCellColour(cell, self.color_yellow)
+						errors += 1
+		return errors
+
+	def CheckAllowedName(self, column_names, state_names):
+		errors = 0
+		cities = []
+		for idx in self.GetColumnIndicesByName(column_names):
+			for index, selected_column in enumerate(islice(self.ws.iter_cols(), idx, idx+1)):
+				for cell in islice(selected_column, 2, None):
+					if cell.value is None:
+						self.fillCellColour(cell, self.color_red)
+						errors += 1
+					elif cell.value.strip() in state_names:
+						cell.value = cell.value.strip()
+					else:
+						self.fillCellColour(cell, self.color_yellow)
+						errors += 1
+		return errors
+
+	def fillCellColour(self, cell, colour):
+		cell.fill = PatternFill(start_color=colour, end_color=colour, fill_type='solid')
+
+	def isNotAvailable(self, value):
+		return value == "NO" or value == "NI" or value == "NA" or value == "N0"  or value == "NP"
--- MediprobeExcelChecker.py
+++ MediprobeExcelChecker.py
@@ -3,14 +3,16 @@ from TextShower import TextShower
 from ExcelFormatter import ExcelFormatter
 
 def PrintResults(errors_found):
-	if errors_found > 0:
+	if errors_found >= 0:
 		print(' ' + str(errors_found) + ' Errors Found.')
-	else:
-		print(" Completed!")
 
 text_shower = 0
 accepted_filename = False
 output_filename = 'output.xlsx'
+errors_found = -1
+
+list_allowed_states = ['Gujarat', 'Madhya Pradesh', 'Maharashtra', 'Rajasthan']
+list_allowed_cities = ['Achalpur', 'Ahmedabad', 'Ahmednagar', 'Ajmer', 'Akluj', 'Akola', 'Alibaug', 'Alwar', 'Amalner', 'Amaravati', 'Ambajogai', 'Ambarnath', 'Ambejogai', 'Amravati', 'Amreli', 'Anand', 'Andheri West', 'Anjangaon', 'Ankleshwar', 'Aravalli', 'Ardhapur', 'Aurangabad', 'Balghat', 'Banaskantha', 'Banswara', 'Baramati', 'Baran', 'Bardoli', 'Barmer', 'Baroda', 'Barshi', 'Bavla', 'Bayad', 'Beed', 'Bhandara', 'Bharatpur', 'Bharuch', 'Bhavnagar', 'Bhayander East', 'Bhilwara', 'Bhiwandi', 'Bhopal', 'Bhor', 'Bhuj', 'Bhuldhana', 'Bhusawal', 'Bikaner', 'Borivali West', 'Borsad', 'Buldana', 'Buldhana', 'Bundi', 'Chalisgaon', 'Chandrapur', 'Chembur', 'Chinchwad', 'Chiplun', 'Chittorgarh', 'Chota Udepur', 'Churu', 'Dadar', 'Dahigaon', 'Dahiwadi', 'Dahod', 'Daman', 'Damoh', 'Daryapur', 'Daund', 'Dausa', 'Deesa', 'Dewas', 'Dhar', 'Dharur', 'Dholka', 'Dhule', 'Dombivli', 'Dungarpur', 'Gadchiroli', 'Gandhidham', 'Gandhinagar', 'Godhra', 'Gondal', 'Gondawale', 'Gondia', 'Guna', 'Gwalior', 'Halol', 'Himmatnagar', 'Hinganghat', 'Hingoli', 'Hosangabad', 'Hupari', 'Ichalkaranji', 'Indapur', 'Indore', 'Islampur', 'Jabalpur', 'Jaipur', 'Jakhangaon', 'Jalgaon', 'Jalna', 'Jamnagar', 'Jath', 'Jaysingpur', 'Jejuri', 'Jhotwara', 'Jodhpur', 'Junagadh', 'Junnar', 'Kalol', 'Kalyan', 'Kalyan West', 'Kandivali East', 'Kandiwali', 'Karad', 'Karjan', 'Kasegaon', 'Katni', 'Khambhat', 'Khandala', 'Khandwa', 'Khargone', 'Khatav', 'Kheda', 'Khopoli', 'Kolhapur', 'Kopargaon', 'Kota', 'Kurduvadi', 'Kurla', 'Kusgaon Budruk', 'Kutch', 'Lashkar', 'Latur', 'Lonand', 'Lonand Satara', 'Lonavala', 'Lonavla', 'Majalgaon', 'Makronia', 'Malvan', 'Mandsaur', 'Mandvi', 'Mangaon', 'Mansa', 'Mehsana', 'Mhaswad', 'Mhow', 'Mira Bhayandar', 'Miraj', 'Modasa', 'Morbi', 'Mumbai', 'Murti', 'Nadiad', 'Nagaur', 'Nagpur', 'Nanded', 'Nandgaon', 'Nandurbar', 'Nanekarwadi', 'Nashik', 'Natepute', 'Navi Mumbai', 'Navsari', 'Neemuch', 'New Panvel ,Raigad', 'Nira', 'Osmanabad', 'PCMC', 'Padra', 'Palanpur', 'Palghar', 'Pali', 'Pandharpur', 'Panvel', 'Panvel West', 'Panvel Navi Mumbai', 'Parali', 'Parbhani', 'Parli', 'Patan', 'Perne', 'Pimpalgaon', 'Porbandar', 'Pune', 'Pusegaon', 'Raigad', 'Rajkot', 'Ratlam', 'Ratnagiri', 'Rewa', 'Sabarkantha', 'Sagar', 'Sangali', 'Sangli', 'Sangola', 'Saswad', 'Satara', 'Satna', 'Satpur Colony', 'Sawai Madhopur', 'Shahdol', 'Shindhudurg', 'Shirala', 'Shirur', 'Shirwal', 'Shrigonda', 'Shrirampur', 'Sikar', 'Sindhudurg', 'Sindkhed Raja', 'Sirsala', 'Solapur', 'Sri Ganganagar', 'Sumerpur', 'Surat', 'Surendra Nagar', 'Tapi', 'Taradgaon', 'Thane', 'Thane West', 'Tongaon', 'Tonk', 'Udaipur', 'Udgir', 'Ujjain', 'Ulhasnagar', 'Urun Islampur', 'Vadodara', 'Vaijapur', 'Valsad', 'Vapi', 'Varvand', 'Vasai', 'Veraval', 'Virar', 'Virar West', 'Vita', 'Walchandnagar', 'Wardha', 'Warwand', 'Wategaon', 'Yavatmal', 'Yawatmal', 'Karad', 'Kota', 'Saswad']
 
 while not accepted_filename:
 	text_shower = TextShower('Enter Filename (.xls/.xlsx)')
@@ -21,24 +23,44 @@ while not accepted_filename:
 if text_shower.getString() is not '':
 	print('Starting Macro on "' + text_shower.getString() + '"...')
 	excel_form = ExcelFormatter(text_shower.getString())
-	print('Checking Address Lines...', end="", flush=True)
+	print('\tChecking Address Lines...', end="", flush=True)
 	errors_found = excel_form.CheckAddressLines(['Hospital_AddressLine1', 'Hospital_AddressLine2', 'Hospital_AddressLine3'])
 	PrintResults(errors_found)
 
-	print('Checking Email Addresses...', end="", flush=True)
+	print('\tChecking Email Addresses...', end="", flush=True)
 	errors_found = excel_form.CheckEmailAddressLines(['Hospital_Email','Hospital_CEOEmail','Hospital_AdminEmail','Hospital_Insurance_Or_TPA_CoordinatorEmail','Hospital_Medical_SuperintendentEmail','Hospital_PromoterEmail'])
 	PrintResults(errors_found)
 
-	print('Checking Phone Numbers...', end="", flush=True)
+	print('\tChecking Phone Numbers...', end="", flush=True)
 	errors_found = excel_form.CheckPhoneNumbersLines(['Hospital_Phone','Hospital_CEOPhone','Hospital_AdminPhone','Hospital_Insurance_Or_TPA_CoordinatorPhone','Hospital_Medical_SuperintendentPhone','Hospital_PromoterPhone'])
 	PrintResults(errors_found)
 
-	print('Checking Fax Numbers...', end="", flush=True)
+	print('\tChecking Fax Numbers...', end="", flush=True)
 	errors_found = excel_form.CheckPhoneNumbersLines(['Hospital_Fax','Hospital_CEOFax','Hospital_PromoterFax'])
 	PrintResults(errors_found)
 
-	print('Checking Latitude Longitudes...', end="", flush=True)
+	print('\tChecking Latitude Longitudes...', end="", flush=True)
 	errors_found = excel_form.CheckLatLonLines(['Hospital_Latitude_Longitude'])
+	PrintResults(errors_found)
+
+	print('\tChecking Titles...', end="", flush=True)
+	errors_found = excel_form.CheckTitleLines(['Hospital_CEOTitle','Hospital_AdminTitle','Hospital_Insurance_Or_TPA_CoordinatorTitle','Hospital_Medical_SuperintendentTitle','Hospital_PromoterTitle'])
+	PrintResults(errors_found)
+
+	print('\tChecking Hours...', end="", flush=True)
+	errors_found = excel_form.CheckHoursLines(['Hospital_OPD_Working_Hours_StartTime','Hospital_OPD_Working_Hours_EndTime'])
+	PrintResults(errors_found)
+
+	print('\tChecking YY/NO/APP...', end="", flush=True)
+	errors_found = excel_form.CheckYYNOAPPLines(['Hospital_HOTA_Registration','Hospital_PNDT_Registration','Hospital_Jci_Accredited','Hospital_Iso_Certified','Hospital_Nabh_Certified','Hospital_Lab_Nabl_Certified','Hospital_Other_Certification'])
+	PrintResults(errors_found)
+
+	print('\tChecking States Names...', end="", flush=True)
+	errors_found = excel_form.CheckAllowedName(['Hospital_State'], list_allowed_states)
+	PrintResults(errors_found)
+
+	print('\tChecking Cities Names...', end="", flush=True)
+	errors_found = excel_form.CheckAllowedName(['Hospital_City'], list_allowed_cities)
 	PrintResults(errors_found)
 
 	print('Saving output to ' + output_filename + '...', end="", flush=True)
